22693238d3c744c0f1f98d11474f4cb3f407ecdb
youngseo <00seo23@gmail.com>
2024-01-23 16:38:23 +0900

    feat: 로그인 페이지 구현-아바타

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,6 +12,7 @@ import NewFillIcon from '@/components/ui/icons/NewFillIcon';
 import { usePathname } from 'next/navigation';
 import ColorButton from '@/components/ui/ColorButton';
 import { useSession, signIn, signOut } from 'next-auth/react';
+import Avatar from '@/components/Avatar';
 
 const menu = [
   {
@@ -34,6 +35,7 @@ const menu = [
 export default function Navbar() {
   const pathName = usePathname();
   const { data: session } = useSession();
+  const user = session?.user;
 
   return (
     <div className="flex justify-between items-center px-6">
@@ -49,11 +51,20 @@ export default function Navbar() {
               </Link>
             </li>
           ))}
-          {session ? (
-            <ColorButton text="Sign out" onClick={() => signOut()} />
-          ) : (
-            <ColorButton text="Sign in" onClick={() => signIn()} />
+          {user && (
+            <li>
+              <Link href={`/user/${user.username}`}>
+                <Avatar image={user.image} />
+              </Link>
+            </li>
           )}
+          <li>
+            {session ? (
+              <ColorButton text="Sign out" onClick={() => signOut()} />
+            ) : (
+              <ColorButton text="Sign in" onClick={() => signIn()} />
+            )}
+          </li>
         </ul>
       </nav>
     </div>
