ca86a56cc786af4e4869dc95f99b01aadc0ba1f1
youngseo <00seo23@gmail.com>
2024-01-24 13:26:03 +0900

    feat: 사이드바 구현 - 스타일링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import FollowingBar from '@/components/FollowingBar';
 import PostList from '@/components/PostList';
 import SideBar from '@/components/SideBar';
-import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 
 export default async function HomePage() {
@@ -15,10 +15,14 @@ export default async function HomePage() {
   }
 
   return (
-    <section>
-      <FollowingBar />
-      <PostList />
-      <SideBar user={user} />
+    <section className="w-full flex flex-col md:flex-row max-w-[850px] p-4">
+      <div className="w-full basis-3/4">
+        <FollowingBar />
+        <PostList />
+      </div>
+      <div className="basis-1/4">
+        <SideBar user={user} />
+      </div>
     </section>
   );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -11,16 +11,20 @@ export default function SideBar({
 }: Props) {
   return (
     <>
-      <div>
-        <div>{image && <Avatar image={image} />}</div>
-        <p>{username}</p>
-        <p>{name}</p>
+      <div className="flex items-center">
+        {image && <Avatar image={image} />}
+        <div className="ml-4">
+          <p className="font-bold">{username}</p>
+          <p className="text-lg text-neutral-500 leading-4">{name}</p>
+        </div>
       </div>
-      <p>
+      <p className="text-sm text-neutral-500 mt-8">
         About ﹒ Help ﹒ Press ﹒ API ﹒ Jobs ﹒ Privacy ﹒ Terms ﹒ Location ﹒
         Language
       </p>
-      <p>@Copyright INSTANTGRAM from METAL</p>
+      <p className="font-bold text-sm mt-8 text-neutral-500">
+        @Copyright INSTANTGRAM from METAL
+      </p>
     </>
   );
 }
