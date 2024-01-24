5b11596f511bd6bdad99a613f738e81a56c07a4c
youngseo <00seo23@gmail.com>
2024-01-24 22:21:39 +0900

    feat: 팔로잉바-컴포넌트

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -1,11 +1,34 @@
 'use client';
 
 import useSWR from 'swr';
+import { DetailUser } from '@/model/user';
+import { PropagateLoader } from 'react-spinners';
+import Link from 'next/link';
+import Avatar from '@/components/Avatar';
 
 export default function FollowingBar() {
-  const { data, isLoading, error } = useSWR('/api/me');
-  console.log(data);
-  // 4. 여기에서, 클라이언트 컴포넌트에서 follwings의 정보를 UI에 보여줌
-  //  (image, username)
-  return <div>FollowingBar</div>;
+  const { data, isLoading: loading, error } = useSWR<DetailUser>('/api/me');
+  const users = data?.following;
+
+  return (
+    <section>
+      {loading ? (
+        <PropagateLoader size={8} color="red" />
+      ) : (
+        (!users || users.length === 0) && <p>{`You don't have following`}</p>
+      )}
+      {users && users.length > 0 && (
+        <ul>
+          {users.map(({ username, image }) => (
+            <li key={username}>
+              <Link href={`/user/${username}`}>
+                <Avatar image={image} highlight />
+                <p>{username}</p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
 }
