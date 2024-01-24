aa02b48699a5c9c6ca401051d4f5f7a89c910eff
youngseo <00seo23@gmail.com>
2024-01-24 21:34:15 +0900

    feat: 팔로잉바- 서비스 구현

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,14 @@
 import { NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 export async function GET(request: Request) {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    return new Response('Authentication Error', { status: 401 });
+  }
+
   return NextResponse.json('Hello Next!!');
 }
