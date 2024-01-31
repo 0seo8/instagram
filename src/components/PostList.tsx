0bcfb5c5f814f6ab50e7e50374cbfec1cdcd9f7b
youngseo <00seo23@gmail.com>
2024-01-31 17:26:47 +0900

    feat: 포스트목록-스타일링

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,20 +1,20 @@
-'use client';
+"use client";
 
-import useSWR from 'swr';
-import { SimplePost } from '@/model/post';
-import { GridLoader } from 'react-spinners';
-import PostListCard from '@/components/PostListCard';
+import useSWR from "swr";
+import { SimplePost } from "@/model/post";
+import { GridLoader } from "react-spinners";
+import PostListCard from "@/components/PostListCard";
 
 export default function PostList() {
   const {
     data: posts,
     isLoading: loading,
     error,
-  } = useSWR<SimplePost[]>('/api/post');
+  } = useSWR<SimplePost[]>("/api/post");
   return (
     <section>
       {loading && (
-        <div>
+        <div className="text-center mt-32">
           <GridLoader color="red" />
         </div>
       )}
@@ -22,7 +22,7 @@ export default function PostList() {
         <ul>
           {posts &&
             posts.map((post) => (
-              <li key={post.id}>
+              <li key={post.id} className="mb-4">
                 <PostListCard post={post} />
               </li>
             ))}
