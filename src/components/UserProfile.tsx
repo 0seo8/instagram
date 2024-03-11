11f88f651e3635a226b25e7b9b99416a79561d3c
0seo8 <00seo23@gmail.com>
2024-03-11 21:21:23 +0900

    feat: 사용자프로필-컴포넌트

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import { ProfileUser } from '@/model/user';
+import Avatar from '@/components/Avatar';
+import FollowButton from '@/components/FollowButton';
 
 type Props = {
   user: ProfileUser;
 };
 
 export default function UserProfile({ user }: Props) {
-  return <p>{user.username}</p>;
+  const { image, username, name, email, followers, following, posts } = user;
+  const info = [
+    { title: 'posts', data: posts },
+    { title: 'followers', data: followers },
+    { title: 'following', data: following },
+  ];
+  return (
+    <section>
+      <Avatar image={image} highlight />
+      <div>
+        <h1>{username}</h1>
+        <FollowButton user={user} />
+        <ul>
+          {info.map(({ title, data }, index) => (
+            <li key={index}>
+              <span>{data}</span>
+              {title}
+            </li>
+          ))}
+        </ul>
+        <p>{name}</p>
+      </div>
+    </section>
+  );
 }
