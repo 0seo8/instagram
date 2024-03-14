59b1d9fd11c583598a64e2f53636ac24586a1a64
0seo8 <00seo23@gmail.com>
2024-03-14 22:40:44 +0900

    feat: force-dynamic

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import UserSearch from '@/components/UserSearch';
 import { Metadata } from 'next';
 
+export const dynamic = 'force-dynamic';
+
 export const metadata: Metadata = {
   title: 'User Search',
   description: 'Search users to follow',
