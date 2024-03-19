f39479eb952419a461b50c2722f6fc4fe89b30f0
0seo8 <00seo23@gmail.com>
2024-03-19 22:54:12 +0900

    feat: 북마크-서버

--- sanity-studio/schemas/user.js
+++ sanity-studio/schemas/user.js
@@ -54,7 +54,7 @@ export default {
       of: [
         {
           type: 'reference',
-          to: [{type: 'user'}],
+          to: [{type: 'post'}],
         },
       ],
       validation: (Rule) => Rule.unique(),
