bff2787aafcc0f22efdbd38e1d3a4a56f4408bc5
0seo8 <00seo23@gmail.com>
2024-04-05 22:31:06 +0900

    feat: 새포스트작성 - 클라이언트

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { AuthUser } from '@/model/user';
 import PostUserAvatar from '@/components/PostUserAvatar';
 import FilesIcon from '@/components/ui/icons/FilesIcon';
 import Button from '@/components/ui/Button';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import GridSpinner from '@/components/ui/GridSpinner';
 
 type Props = {
   user: AuthUser;
@@ -14,6 +16,10 @@ type Props = {
 export default function NewPost({ user: { username, image } }: Props) {
   const [dragging, setDragging] = useState(false);
   const [file, setFile] = useState<File>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string>('');
+  const textRef = useRef<HTMLTextAreaElement>(null);
+  const router = useRouter();
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     const files = e.target?.files;
@@ -39,10 +45,42 @@ export default function NewPost({ user: { username, image } }: Props) {
       setFile(files[0]);
     }
   };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!file) return;
+    setLoading(true);
+
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('text', textRef.current?.value ?? '');
+
+    fetch('/api/posts/', { method: 'POST', body: formData })
+      .then((res) => {
+        if (!res.ok) {
+          setError(`${res.status} ${res.statusText}`);
+          return;
+        }
+        router.push('/');
+      })
+      .catch((err) => setError(err.toString()))
+      .finally(() => setLoading(false));
+  };
+
   return (
     <section className="w-full max-w-xl flex flex-col items-center mt-6">
+      {loading && (
+        <div className="absolute inset-0 z-20 text-center pt-[30%] bg-sky-500/20">
+          <GridSpinner />
+        </div>
+      )}
+      {error && (
+        <p className="w-full bg-red-100 text-red-600 text-center p-4 mb-4 font-bold">
+          {error}
+        </p>
+      )}
       <PostUserAvatar image={image ?? ''} username={username} />
-      <form className="w-full flex flex-col mt-2">
+      <form className="w-full flex flex-col mt-2" onSubmit={handleSubmit}>
         <input
           className="hidden"
           name="input"
@@ -90,6 +128,7 @@ export default function NewPost({ user: { username, image } }: Props) {
           required
           rows={10}
           placeholder="Write a caption..."
+          ref={textRef}
         />
         <Button text="Publish" onClick={() => {}} />
       </form>
